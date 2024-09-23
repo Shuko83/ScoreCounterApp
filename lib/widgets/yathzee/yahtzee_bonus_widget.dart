54ba896b3bdd6a1180d24b3684c8915600258840
Fabien <digrande.fabien@gmail.com>
2024-09-24 01:59:11 +0200

    Ajout de l'implémentation du listener. Modif du rendu visuel du widget

--- lib/widgets/yathzee/yahtzee_bonus_widget.dart
+++ lib/widgets/yathzee/yahtzee_bonus_widget.dart
@@ -2,22 +2,44 @@
 import 'package:flutter/material.dart';
 import 'package:score_counter_app/controllers/yahtzee_controller.dart';
 
-class YahtzeeBonusWidget extends StatelessWidget{
+class YahtzeeBonusWidget extends StatefulWidget{
   const YahtzeeBonusWidget({
     super.key,
     required this.controller,
-  });
+  }
+  );
 
   final YahtzeeController controller;
 
   @override
+  State<YahtzeeBonusWidget> createState() => _YahtzeeBonusWidgetState();
+  
+}
+
+class _YahtzeeBonusWidgetState extends State<YahtzeeBonusWidget> implements ValuesListener{
+  var pointNeeded = "0";
+  @override
+  void onValueChanged(){
+   setState(() {
+    pointNeeded = widget.controller.distanceToBonus().toString();
+   }); 
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if(controller.bonusSuccess()){
-      return const Icon(Icons.check_circle,
-      color: Colors.green, );
-    }else {
-    return Text(
-      controller.distanceToBonus().toString(),
+    widget.controller.registerValuesListeners(this);
+    if(widget.controller.bonusSuccess()){
+      return const Row(
+        children: [
+          Text("Bonus achieve."),
+          Spacer(),
+          Icon(Icons.check_circle,
+            color: Colors.green, 
+          ),
+        ],
+      );
+    }else {   
+    return Text('You need $pointNeeded more points to have bonus.'
     );
     }
   }
